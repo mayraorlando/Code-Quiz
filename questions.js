eb3efa5ae3a8783f37e42fb8ee97791daf5e9489
Mayra Orlando <mayraorlando2014@gmail.com>
2021-03-29 17:53:12 -0400

    homework submission

---
+++ questions.js
@@ -0,0 +1,28 @@
+// Quiz Questions
+var questions = [
+    {
+        title: "Commonly Used data types DO NOT include:",
+        choices: ["stings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store _____.",
+        choices:["Numbers and Strings", "Other Arrays","Boolean","All the abaove"],
+        answer: "Numbers and Strings"
+    },
+    {
+        title: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["Commas", "Curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        choices: ["JavaScript", "terminal/bash", "for loop", "console.log"],
+        answer: "console.log"
+    },
+  ]
